acec33c1b19530efc27194d71a98a335e6cf1281
Vimal Tiwari <vimaltiwari2612@gmail.com>
2019-04-19 11:49:08 +0530

    Update JavaIndentator.java

--- JavaIndentator.java
+++ JavaIndentator.java
@@ -6,8 +6,9 @@ public class JavaIndentator implements AbstractIndentator{
 	private ArrayList<String> parsedLines;
 	private static JavaIndentator INSTANCE;
 	private RandomAccessFile file;
+	
 	private JavaIndentator(){
-
+		//can't use new operator now
 	}
 
 	public static JavaIndentator getInstance(){
@@ -15,29 +16,29 @@ public class JavaIndentator implements AbstractIndentator{
 			INSTANCE = new JavaIndentator();
 		return INSTANCE;
 	}
-
-	private void push(String element){
-		symbols.add(element); 
-	}
-
-	private String pop(){
-		return symbols.get(symbols.size() - 1); 
-	}
-
+	
 	public void parse(){
 		try{
 			prepareSymbolTable();
 			indentCodeUsingSymbolTable();
 			reWriteTheFile();
+			System.out.println("Parsing Completed!");
 		}catch(Exception e){
-			System.out.println(e.getMessage());
+			System.out.println("Error while parsing === "+e.getMessage());
+		}finally{
+			try{
+				//free the resources
+				this.file.close();
+			}catch(IOException io){
+				System.out.println("Error while closing file === "+io.getMessage());
+			}
 		}
 	}
 	
 	public void setFileStream(RandomAccessFile file){
 		this.file = file;
 	}
-
+	//symbol table formation
 	private void prepareSymbolTable() throws Exception{
 		this.symbols = new ArrayList<String>();
 		while(this.file.getFilePointer() < this.file.length()){
@@ -64,21 +65,22 @@ public class JavaIndentator implements AbstractIndentator{
 		}
 		return tabs;
 	}
-
+	
+	//main parsing logic
+	//lexical anaylsis
 	private void indentCodeUsingSymbolTable(){
 		parsedLines = new ArrayList<String>();
-		Integer tabCount = 0;
 		LinkedList<String> localStack = new LinkedList<String>();
 		String currentLine = "";
 		for(String element : this.symbols){
 			element = element.trim();
-			if(!element.contains("{") && !element.contains(";")){
-				if(element.contains("}")) {
+			if(!element.contains("{") && !element.contains(";") && !element.contains("/*")){
+				if(element.contains("}") || element.contains("*/")) {
 					String topElement = localStack.peekLast();
-					if(topElement!=null && topElement.contains("{")){
+					if(topElement!=null && (topElement.contains("{") || topElement.contains("/*"))){
 						localStack.removeLast();
 						currentLine+=element +" \n";
-						parsedLines.add(this.getIndentationForThisLine(localStack.size())+currentLine); 
+						this.addLine(currentLine, localStack.size());
 						currentLine="";
 						continue;
 					}
@@ -88,12 +90,16 @@ public class JavaIndentator implements AbstractIndentator{
 			else{	
 				
 				currentLine+=element +" \n";
-				parsedLines.add(this.getIndentationForThisLine(localStack.size())+currentLine); 
+				this.addLine(currentLine, localStack.size()); 
 				currentLine="";
-				if(element.contains("{")) {
+				if(element.contains("{") || element.contains("/*")) {
 					localStack.add(element);
 				}
 			}
 		}
+	}
+	//add it to global list
+	private void addLine(String currentLine, Integer tabCount){
+		this.parsedLines.add(this.getIndentationForThisLine(tabCount)+currentLine); 
 	}
 }
